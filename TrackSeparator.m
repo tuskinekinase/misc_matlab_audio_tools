5f57acd41b47eeec70b42ebb9cdf69105e8f8bb6
ugsnowbootp <76928229+tuskinekinase@users.noreply.github.com>
2024-08-29 16:22:04 +0800

    Add files via upload
    TrackSeparator.m can now use uigetfile to get custom files and separate any number of channels using a loop.

--- TrackSeparator.m
+++ TrackSeparator.m
@@ -1,5 +1,17 @@
-[Y,fs]=audioread('C:\Users\emily\Documents\MATLAB\DeepSqueak_test\Tsinghua\KM\Multi\220813_001_0002.WAV');
-audiowrite('220813_KM_0002_ch1.wav',Y(:,1),fs,'BitsPerSample',32);
-audiowrite('220813_KM_0002_ch2.wav',Y(:,2),fs,'BitsPerSample',32);
-audiowrite('220813_KM_0002_ch3.wav',Y(:,3),fs,'BitsPerSample',32);
-% audiowrite('220810_TJ_0006_ch4.wav',Y(:,4),fs,'BitsPerSample',32);
+% Separate multichannel audio into mono tracks.
+[file,location]=uigetfile('*.wav');
+filefull = fullfile(location,file);
+[p,f,e]=fileparts(filefull);
+
+[Y,fs]=audioread(filefull);
+
+tracknum=length(Y(1,:));
+
+s=0;
+for i=1:tracknum
+    if i-s>=0
+    trackname=strcat([f,'_ch',num2str(i),'.WAV']);
+    audiowrite(trackname,Y(:,i),fs,'BitsPerSample',32);
+    s=s+1;
+    end
+end
